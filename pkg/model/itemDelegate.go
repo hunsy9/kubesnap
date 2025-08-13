d9afac2dae1b2ad21006cba71d52f8f83bbb07fe
Seunghun Yu <juniper0917@gmail.com>
2025-08-13 19:23:31 +0900

    implement itemDelegate

--- pkg/model/itemDelegate.go
+++ pkg/model/itemDelegate.go
@@ -1,7 +1,36 @@
 package model
 
+import (
+	"fmt"
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"io"
+	"strings"
+)
+
 type Item string
 
 func (i Item) FilterValue() string { return string(i) }
 
 type ItemDelegate struct{}
+
+func (d ItemDelegate) Height() int                             { return 1 }
+func (d ItemDelegate) Spacing() int                            { return 0 }
+func (d ItemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
+func (d ItemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(Item)
+	if !ok {
+		return
+	}
+
+	str := fmt.Sprintf("%s", i)
+
+	fn := itemStyle.Render
+	if index == m.Index() {
+		fn = func(s ...string) string {
+			return selectedItemStyle.Render("> " + strings.Join(s, " "))
+		}
+	}
+
+	fmt.Fprint(w, fn(str))
+}
