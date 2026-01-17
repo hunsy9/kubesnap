882b90443f3c78232ca0f1de94e4a587f778168e
Seunghun Yu <juniper0917@gmail.com>
2026-01-17 21:56:31 +0900

    chore: organize constants

--- pkg/constant/constant.go
+++ pkg/constant/constant.go
@@ -1,9 +1,12 @@
 package constant
 
 const (
-	DefaultSwitchingContextHeaderMessage        = "Select a Context"
-	DefaultKubeConfigLocation            string = "/.kube/config"
+	// UI Configuration
+	DefaultSwitchingContextHeaderMessage string = "Select a Context"
 	DefaultWidth                         int    = 10
 	ListHeight                           int    = 15
 	DefaultThemeColor                    string = "#8b11f5ff"
+
+	// File Paths
+	DefaultKubeConfigLocation string = "/.kube/config"
 )
