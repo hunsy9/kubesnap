98e20cd5beee4fc88ba0e7b5c75c09384806fcb8
Seunghun Yu <juniper0917@gmail.com>
2026-01-17 17:56:48 +0900

    chore: add DefaultSwitchingContextHeaderMessage constant

--- pkg/constant/constant.go
+++ pkg/constant/constant.go
@@ -1,8 +1,9 @@
 package constant
 
 const (
-	DefaultKubeConfigLocation string = "/.kube/config"
-	DefaultWidth              int    = 10
-	ListHeight                int    = 15
-	DefaultThemeColor         string = "#8b11f5ff"
+	DefaultSwitchingContextHeaderMessage        = "Select a Context"
+	DefaultKubeConfigLocation            string = "/.kube/config"
+	DefaultWidth                         int    = 10
+	ListHeight                           int    = 15
+	DefaultThemeColor                    string = "#8b11f5ff"
 )
