bf380b67b25136c520a9d2615a0e3c3dc41f5d86
hunsy9 <juniper0917@gmail.com>
2026-01-24 06:53:47 +0000

    feat: implement ks(view cluster info) command

--- cmd/info.go
+++ cmd/info.go
@@ -3,12 +3,81 @@ package main
 import (
 	"fmt"
 	"io"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	c "github.com/hunsy9/kubesnap/pkg/constant"
+	"github.com/hunsy9/kubesnap/pkg/envutil"
+	ks "github.com/hunsy9/kubesnap/pkg/model/kubestatus"
+	"github.com/pkg/errors"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 type InfoCmd struct{}
 
 func (InfoCmd) Run(stdout, stderr io.Writer) error {
-	info := "kubesnap info"
-	fmt.Fprintln(stdout, info)
+
+	// get client set via kubeconfig file
+
+	kubeConfigPath := os.Getenv("HOME") + c.DefaultKubeConfigLocation
+	filepath := envutil.GetEnvOrDefault("KUBECONFIG", kubeConfigPath)
+
+	clientSet, err := getClientSet(filepath)
+	if err != nil {
+		return errors.WithMessage(err, "unexpected error occured")
+	}
+
+	// get current cluster's name
+
+	config, err := clientcmd.LoadFromFile(clientcmd.RecommendedHomeFile)
+	if err != nil {
+		return errors.New("failed to load config from kubeconfig")
+	}
+
+	clusterName := config.CurrentContext
+	namespace := GetCurrentNamespace()
+
+	// get current cluster's api server endpoint
+
+	endpoint, err := getCurrentApiServerEndpoint()
+	if err != nil {
+		return errors.Cause(err)
+	}
+
+	md := ks.NewStatusModel(clientSet, clusterName, namespace, endpoint)
+	p := tea.NewProgram(md)
+
+	_, err = p.Run()
+	if err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+
 	return nil
+}
+
+func getCurrentApiServerEndpoint() (string, error) {
+	config, err := clientcmd.LoadFromFile(clientcmd.RecommendedHomeFile)
+	if err != nil {
+		return "", errors.New("failed to load config from kubeconfig")
+	}
+
+	currentContext := config.CurrentContext
+	ctx, exists := config.Contexts[currentContext]
+
+	if !exists {
+		return "", errors.Errorf("context '%s' not found", currentContext)
+	}
+
+	cluster, exists := config.Clusters[ctx.Cluster]
+
+	if !exists {
+		return "", errors.Errorf("cluster '%s' not found", ctx.Cluster)
+	}
+
+	if cluster.Server == "" {
+		return "", errors.Errorf("api server endpoint is empty for cluster '%s'", ctx.Cluster)
+	}
+
+	return cluster.Server, nil
 }
--- pkg/model/kubestatus/kubestatus.go
+++ pkg/model/kubestatus/kubestatus.go
@@ -1,17 +1,57 @@
 package kubestatus
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
-	"github.com/hunsy9/kubesnap/pkg/constant"
+	c "github.com/hunsy9/kubesnap/pkg/constant"
 	authenticationv1 "k8s.io/api/authentication/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
-var (
-	spinnerStyle = lipgloss.NewStyle().Foreground(lipgloss.Color(constant.DefaultThemeColor))
-)
+type ApiHealthMsg struct {
+	Msg string
+}
+
+type AuthInfoMsg struct {
+	Msg *authenticationv1.SelfSubjectReview
+	Err error
+}
+
+func (m *StatusModel) checkApiHealth() tea.Msg {
+	start := time.Now()
+	_, err := m.clientSet.Discovery().RESTClient().Get().AbsPath("/healthz").DoRaw(context.TODO())
+	elapsed := time.Since(start).Milliseconds()
+	if err != nil {
+		return ApiHealthMsg{
+			Msg: errorStyle.Render(fmt.Sprintf("● InActive (%s)", err.Error())),
+		}
+	}
+
+	activeMsg := activeStyle.Render(fmt.Sprintf("● Active (%vms)", elapsed))
+	return ApiHealthMsg{
+		Msg: activeMsg,
+	}
+}
+
+func (m *StatusModel) getAuthInfo() tea.Msg {
+	ssr := &authenticationv1.SelfSubjectReview{}
+	result, err := m.clientSet.AuthenticationV1().SelfSubjectReviews().Create(context.TODO(), ssr, metav1.CreateOptions{})
+
+	if err != nil {
+		return AuthInfoMsg{
+			Err: err,
+		}
+	}
+	return AuthInfoMsg{
+		Msg: result,
+	}
+}
 
 type StatusModel struct {
 	spinner         spinner.Model // spinner model
@@ -20,6 +60,7 @@ type StatusModel struct {
 	namespace       string
 	endpoint        string
 	auth            *authenticationv1.SelfSubjectReview
+	apiStatus       string
 	isHealthLoading bool
 	isAuthLoading   bool
 }
@@ -40,5 +81,83 @@ func NewStatusModel(clientSet *kubernetes.Clientset, clusterName string, namespa
 }
 
 func (m *StatusModel) Init() tea.Cmd {
-	return nil
+	return tea.Batch(
+		m.spinner.Tick,
+		m.checkApiHealth,
+		m.getAuthInfo,
+	)
+}
+
+func (m *StatusModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch keypress := msg.String(); keypress {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		}
+	case spinner.TickMsg:
+		var spinCmd tea.Cmd
+		m.spinner, spinCmd = m.spinner.Update(msg)
+		cmds = append(cmds, spinCmd)
+
+	case ApiHealthMsg:
+		m.apiStatus = msg.Msg
+		m.isHealthLoading = false
+
+	case AuthInfoMsg:
+		m.auth = msg.Msg
+		m.isAuthLoading = false
+	}
+
+	return m, tea.Batch(cmds...)
+}
+
+func (m *StatusModel) View() string {
+
+	var apiStatusView string
+
+	if m.isHealthLoading {
+		apiStatusView = m.spinner.View()
+	} else {
+		apiStatusView = m.apiStatus
+	}
+
+	var userView, groupView string
+
+	if m.isAuthLoading {
+		userView = m.spinner.View()
+		groupView = m.spinner.View()
+	} else if m.auth != nil {
+		userView = fmt.Sprintf("%v", m.auth.Status.UserInfo.Username)
+		groupView = fmt.Sprintf("%v", m.auth.Status.UserInfo.Groups)
+	} else {
+		userView = iconStyle.Render("-")
+		groupView = iconStyle.Render("-")
+	}
+
+	clusterConnection := fmt.Sprintf(" %s Cluster: %s\n %s API Server: %s\n    └─ Status: %s\n",
+		iconStyle.Render(c.Bullet),
+		m.clusterName, iconStyle.Render(c.Bullet),
+		urlStyle.Render(m.endpoint),
+		apiStatusView)
+
+	clusterAuth := fmt.Sprintf(" %s User: %s\n %s Groups: %s\n %s Namespace: %s\n",
+		iconStyle.Render(c.Bullet),
+		userView,
+		iconStyle.Render(c.Bullet),
+		groupView,
+		iconStyle.Render(c.Bullet),
+		m.namespace)
+
+	layout := boxStyle.Render(
+		lipgloss.JoinVertical(
+			lipgloss.Left,
+			headerStyle.Render("CLUSTER CONNECTION\n"),
+			clusterConnection,
+			headerStyle.Render("AUTH & SCOPE\n"),
+			clusterAuth),
+	)
+	return layout
 }
