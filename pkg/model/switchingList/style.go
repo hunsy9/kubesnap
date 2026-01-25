e01a5f82d0f00b0de74e3c53d0a6a9b6b99de913
hunsy9 <juniper0917@gmail.com>
2026-01-25 13:35:13 +0000

    style: modify selectedItemStyle of switchingList Model

--- pkg/model/switchingList/style.go
+++ pkg/model/switchingList/style.go
@@ -10,7 +10,7 @@ var (
 	boxStyle          = lipgloss.NewStyle().Border(lipgloss.RoundedBorder()).BorderForeground(lipgloss.Color(constant.DefaultThemeColor)).Padding(0, 2).Margin(0, 0, 1, 0)
 	titleStyle        = lipgloss.NewStyle().MarginTop(1).Bold(true).Italic(true).Foreground(lipgloss.Color(constant.DefaultThemeColor))
 	itemStyle         = lipgloss.NewStyle().PaddingLeft(2)
-	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(1).Foreground(lipgloss.Color(constant.DefaultThemeColor))
+	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(1).Bold(true).Foreground(lipgloss.Color(constant.DefaultThemeColor))
 	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(2)
 	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(2).PaddingBottom(1)
 	spinnerStyle      = lipgloss.NewStyle().Foreground(lipgloss.Color(constant.DefaultThemeColor))
