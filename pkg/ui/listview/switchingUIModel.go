2161042e2369077ce852b2cfe7ea72d26192a640
hunsy9 <juniper0917@gmail.com>
2026-02-09 12:53:37 +0000

    feat: merge rename ui to switching ui

--- pkg/ui/listview/switchingUIModel.go
+++ pkg/ui/listview/switchingUIModel.go
@@ -8,9 +8,54 @@ import (
 	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/bubbles/spinner"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	c "github.com/hunsy9/kubesnap/pkg/constant"
+	"k8s.io/client-go/tools/clientcmd"
 )
+
+func (m *SwitchingUIModel) RenameOperation(newName string) tea.Cmd {
+	return func() tea.Msg {
+		if m.choice == newName {
+			return RenameResultMsg{Err: nil}
+		}
+
+		loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+		config, err := loadingRules.Load()
+		if err != nil {
+			return RenameResultMsg{Err: err}
+		}
+
+		if _, exists := config.Contexts[newName]; exists {
+			return RenameResultMsg{Err: fmt.Errorf("context named '%s' already exists", newName)}
+		}
+
+		ctx, exists := config.Contexts[m.choice]
+		if !exists {
+			return RenameResultMsg{Err: fmt.Errorf("context '%s' not found", m.choice)}
+		}
+
+		config.Contexts[newName] = ctx
+		delete(config.Contexts, m.choice)
+
+		if config.CurrentContext == m.choice {
+			config.CurrentContext = newName
+		}
+
+		if err := clientcmd.ModifyConfig(loadingRules, *config, true); err != nil {
+			return RenameResultMsg{Err: err}
+		}
+
+		return RenameResultMsg{OriginalName: m.choice, NewName: newName, Err: nil}
+	}
+}
+
+type RenameResultMsg struct {
+	OriginalName string
+	NewName      string
+	Err          error
+}
 
 type OperationResultMsg struct {
 	Err error
@@ -19,13 +64,15 @@ type OperationResultMsg struct {
 type SwitchingOperation func(param string) tea.Cmd // switching function executed by UIModel
 
 type SwitchingUIModel struct {
-	spinner   spinner.Model      // spinner model
-	list      list.Model         // list model
-	tag       string             // operation tag
-	choice    string             // target kubernetes cluster which user selected
-	switching bool               // variable which shows state of changing context/namespace
-	operation SwitchingOperation // switching function executed by UIModel
-	output    string             // success output message for context switching
+	spinner            spinner.Model      // spinner model
+	list               list.Model         // list model
+	tag                string             // operation tag
+	choice             string             // target kubernetes cluster which user selected
+	switching          bool               // variable which shows state of changing context/namespace
+	renaming           bool               // variable which shows state of renaming context
+	switchingOperation SwitchingOperation // switching function executed by UIModel
+	output             string             // success output message for context switching
+	textInput          textinput.Model
 }
 
 func NewSwitchingUIModel(items []list.Item, title string, tag string) *SwitchingUIModel {
@@ -58,7 +105,12 @@ func NewSwitchingUIModel(items []list.Item, title string, tag string) *Switching
 	sp.Spinner = spinner.Dot
 	sp.Style = spinnerStyle
 
-	return &SwitchingUIModel{spinner: sp, list: l, tag: tag}
+	ti := textinput.New()
+	ti.Placeholder = "New Context Name"
+	ti.CharLimit = 156
+	ti.Width = 150
+
+	return &SwitchingUIModel{spinner: sp, list: l, tag: tag, textInput: ti}
 }
 
 func (m *SwitchingUIModel) Init() tea.Cmd {
@@ -70,6 +122,30 @@ func (m *SwitchingUIModel) IsCustomKeyEnablementInValid() bool {
 }
 
 func (m *SwitchingUIModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	// 1. Text Input(Rename) Mode
+	if m.renaming {
+		switch msg := msg.(type) {
+		case tea.KeyMsg:
+			switch msg.String() {
+			case "enter":
+				return m, m.RenameOperation(m.textInput.Value())
+			case "esc":
+				m.renaming = false
+				m.textInput.Blur()
+				return m, nil
+			}
+		case RenameResultMsg:
+			m.renaming = false
+			return m, func() tea.Msg { return msg }
+		}
+
+		m.textInput, cmd = m.textInput.Update(msg)
+		return m, cmd
+	}
+
+	// 2. Context/Namespace Switching Mode
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
 		return m, nil
@@ -87,7 +163,7 @@ func (m *SwitchingUIModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			}
 
 			m.switching = true
-			return m, tea.Batch(m.spinner.Tick, m.operation(m.choice))
+			return m, tea.Batch(m.spinner.Tick, m.switchingOperation(m.choice))
 		case "d":
 			if m.IsCustomKeyEnablementInValid() {
 				break
@@ -100,10 +176,17 @@ func (m *SwitchingUIModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			if m.IsCustomKeyEnablementInValid() {
 				break
 			}
-			currentWidth := m.list.Width()
-			items := m.list.Items()
 
-			return NewRenamingModel(m, items, currentWidth, RenameOperation), nil
+			i, ok := m.list.SelectedItem().(Item)
+			if ok {
+				m.choice = string(i.Name)
+				m.textInput.SetValue(m.choice)
+				m.textInput.Focus()
+				m.textInput.CursorEnd()
+				m.renaming = true
+			}
+
+			return m, nil
 		}
 	case OperationResultMsg:
 		m.switching = false
@@ -132,7 +215,7 @@ func (m *SwitchingUIModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		if msg.Err != nil {
 			m.output = fmt.Sprintf("Error renaming: %v\n", msg.Err)
 		} else {
-			m.output = "Renamed context successfully.\n"
+			m.output = fmt.Sprintf("Renamed context successfully.\nbefore: %s\nafter: %s\n", msg.OriginalName, msg.NewName)
 		}
 		return m, tea.Quit
 
@@ -140,7 +223,6 @@ func (m *SwitchingUIModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		return m, tea.Quit
 	}
 
-	var cmd tea.Cmd
 	if m.switching {
 		m.spinner, cmd = m.spinner.Update(msg)
 		return m, cmd
@@ -151,16 +233,28 @@ func (m *SwitchingUIModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m *SwitchingUIModel) View() string {
+	listView := boxStyle.Render(m.list.View())
+
 	if m.switching {
 		switchingMsg := fmt.Sprintf("%s Switching %s to %s...", m.spinner.View(), m.tag, m.choice)
 		m.list.Title = switchingMsg
 	}
 
-	return boxStyle.Render(m.list.View())
+	if m.renaming {
+		inputView := fmt.Sprintf(
+			"\n  Rename %s to:\n  %s\n\n  %s",
+			targetContextStyle.Render(m.choice),
+			m.textInput.View(),
+			quitRenameModeFooterStyle.Render(c.QuitRenameMode),
+		)
+		return lipgloss.JoinVertical(lipgloss.Left, listView, inputView)
+	}
+
+	return listView
 }
 
 func (m *SwitchingUIModel) SetOperationFunc(operation SwitchingOperation) {
-	m.operation = operation
+	m.switchingOperation = operation
 }
 
 func (m *SwitchingUIModel) GetOutput() string {
